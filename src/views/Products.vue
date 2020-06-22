c0a81bc3b2c2b44953b8c1daa7216dee135444e6
Roy Ketelaar <royketelaar@gmail.com>
2020-06-22 23:13:36 +0200

    added cool styling to cards

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="products">
-    <h1>Products</h1>
-    <ul>
-      <li v-for="product in products" :key="product.id">
-        {{ product.name }}
-      </li>
-    </ul>
+  <div class="products container mx-auto mt-20">
+    <h1 class="text-4xl font-bold mb-8">Products</h1>
+    <div class="grid grid-rows-4 grid-flow-col gap-12">
+      <div v-for="product in products" :key="product.id"
+        class="shop-item shadow hover:shadow-lg transition cursor-pointer">
+        <img 
+          :src="`http://localhost:1337${product.image.formats.medium.url}`"
+          class="mb-2"
+          alt="">
+        <h2 class="text-teal-700 text-lg font-bold px-4 pt-2">{{ product.name }}</h2>
+        <span class="price text-2xl block px-4 pb-4">
+          € {{ product.price }}
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -25,3 +33,19 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+  img {
+    width: 100%;
+    height: 400px;
+    object-fit: cover;
+    object-position: center;
+  }
+  .shop-item {
+
+  }
+
+  .transition {
+    transition: .2s;
+  }
+</style>
