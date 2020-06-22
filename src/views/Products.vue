ca96a2ff65d00e699cae6cba3b1a5b469eac0e16
Roy Ketelaar <royketelaar@gmail.com>
2020-06-22 21:49:23 +0200

    added list of products

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="products">
-    <h1>This is a products page</h1>
+    <h1>Products</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.name }}
+      </li>
+    </ul>
   </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data () {
+    return {
+      products: null
+    }
+  },
+  mounted () {
+    axios
+      .get('http://localhost:1337/Products')
+      .then(response => (this.products = response.data))
+  }
+};
+</script>
